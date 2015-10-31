f172d32a6c62bdbaa4b438281e9331fd0c205b1b
Christophe Vanlancker <christophe.vanlancker@inuits.eu>
2015-10-31 14:35:53 +0100

    Make PHP Code Sniffer happy by adding doc
    Signed-off-by: Christophe Vanlancker <christophe.vanlancker@inuits.eu>

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
 
+/**
+ * My Website Project
+ *
+ * PHP version 5
+ *
+ * @category Demo
+ * @package  None
+ * @author   Christophe Vanlancker <carroarmato0@gmail.com>
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link     http://carroarmato0.be
+ *
+ */
+
+
   echo "Hello World";
